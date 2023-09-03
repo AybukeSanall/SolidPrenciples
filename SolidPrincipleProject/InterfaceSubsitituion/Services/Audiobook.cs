812a7fce7307da00478e0b9857d93edd655960c4
Aybüke Şanal <aybukesnl7@gmail.com>
2023-09-03 17:31:22 +0300

    ISP added

---
+++ SolidPrincipleProject/InterfaceSubsitituion/Services/Audiobook.cs
@@ -0,0 +1,22 @@
+﻿using SolidPrincipleProject.SingleResponsibility.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrincipleProject.InterfaceSubsitituion.Services
+{
+    public class Audiobook : Book, IBookListenable,IBookDownloadable
+    {
+        public void Download()
+        {
+            Console.WriteLine($"Downloading audiobook: {Name} by {Author}");
+        }
+
+        public void Listen()
+        {
+            Console.WriteLine($"Listening to Audiobook: {Name} by {Author}");
+        }
+    }
+}
---
+++ SolidPrincipleProject/InterfaceSubsitituion/Services/EBook.cs
@@ -0,0 +1,17 @@
+﻿using SolidPrincipleProject.SingleResponsibility.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrincipleProject.InterfaceSubsitituion.Services
+{
+    public class EBook : Book, IBookDownloadable
+    {
+        public void Download()
+        {
+            Console.WriteLine($"Downloading eBook: {Name} by {Author}");
+        }
+    }
+}
