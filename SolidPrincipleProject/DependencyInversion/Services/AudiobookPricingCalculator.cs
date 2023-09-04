c8b9e949517a332d4e65a70cdf167c2ffdc5cb9d
Aybüke Şanal <aybukesnl7@gmail.com>
2023-09-04 11:50:21 +0300

    DIP added

---
+++ SolidPrincipleProject/DependencyInversion/Services/AudiobookPricingCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrincipleProject.DependencyInversion.Services
+{
+    public class AudiobookPricingCalculator : IBookPricingCalculator
+    {
+        public decimal CalculatePrice(decimal basePrice)
+        {
+           return basePrice* 0.8m;
+        }
+    }
+}
---
+++ SolidPrincipleProject/DependencyInversion/Services/BookStore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrincipleProject.DependencyInversion.Services
+{
+    public class BookStore
+    {
+        private readonly IBookPricingCalculator _bookPricingCalculator;
+        public BookStore(IBookPricingCalculator bookPricingCalculator)
+        {
+            _bookPricingCalculator = bookPricingCalculator;
+        }
+
+        public decimal CalculateTotalPrice(decimal basePrice)
+        {
+            
+            return _bookPricingCalculator.CalculatePrice(basePrice);
+        }
+    }
+}
